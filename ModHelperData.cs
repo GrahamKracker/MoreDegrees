ecaa05d7124eddd82769b25eb4d086d6a19a0842
Not-Graham <gjguzynski@gmail.com>
2022-11-20 01:45:30 -0500

    fix description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string Name = "MoreDegrees";
-    public const string Description = "MoreDegrees by GrahamKraker";
+    public const string Description = "Adds infinite paragon degrees. The coefficients used in the equation to determine the amount of sacrifices needed per degree are able to be changed in mod settings. Modded paragons should be compatible. Engineer paragon sentries are currently unaffected by degrees above 100. The max degree paragons can be set to is able to be changed in settings. Insanely high degrees cause damage to become negative, this is a flaw within btd6 that I can't fix.";
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKraker";
     public const string RepoName = "MoreDegrees";
